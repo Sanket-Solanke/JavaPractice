008e05a16f80b0d52d5d78cb505c9dc6afe3f470
Sanket-Solanke <sanket.ksolanke@gmail.com>
2023-11-12 10:28:35 +0530

    First commit

---
+++ HackathonPractice/src/com/sunbeam/DbUtil.java
@@ -0,0 +1,26 @@
+package com.sunbeam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbUtil {
+	public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/hackathon";
+	public static final String DB_USER = "D1_80439_Sanket";
+	public static final String DB_PASSSWD = "80439";
+
+	static {
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	public static Connection getConnection() throws Exception {
+		Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSSWD);
+		return con;
+	}
+}
+
---
+++ HackathonPractice/src/com/sunbeam/User.java
@@ -0,0 +1,82 @@
+package com.sunbeam;
+
+import java.util.Objects;
+
+public class User {
+	int id;
+	String firstName;
+	String lastName;
+	String email;
+	String password;
+	String mobile;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, firstName, id, lastName, mobile, password);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName) && id == other.id
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(mobile, other.mobile)
+				&& Objects.equals(password, other.password);
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", mobile=" + mobile + "]";
+	}
+	public User(int id, String firstName, String lastName, String email, String password, String mobile) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.mobile = mobile;
+	}
+	
+	
+	
+}
